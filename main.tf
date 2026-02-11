fd8421df2881cf3a7c7fd75180dfa6199c728eb9
Papas412 <gpapas412@gmail.com>
2026-02-11 16:06:08 +0000

    added lamda and event trigger

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 resource "aws_s3_bucket" "bronze_bucket" {
   bucket = var.bronze_bucket_name
-  versioning {
-    enabled = true
+}
+
+resource "aws_s3_bucket_versioning" "bronze_bucket_versioning" {
+  bucket = aws_s3_bucket.bronze_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
@@ -23,7 +27,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "bronze_bucket_lifecycle" {
   }
 }
 
-resource "aws-iam-role" "weather_raw_lambda_role" {
+resource "aws_iam_role" "weather_raw_lambda_role" {
   name = "weather-raw-lambda-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -40,36 +44,61 @@ resource "aws-iam-role" "weather_raw_lambda_role" {
 }
 
 
-resource "aws-iam-policy" "weather_raw_lambda_policy" {
+resource "aws_iam_policy" "weather_raw_lambda_policy" {
   name = "weather-raw-lambda-policy"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "s3:PutObject"
-        Effect = "Allow"
+        Action   = "s3:PutObject"
+        Effect   = "Allow"
         Resource = "arn:aws:s3:::${var.bronze_bucket_name}/*"
       }
     ]
   })
 }
 
-resource "aws-iam-role-policy-attachment" "weather_raw_lambda_role_policy_attachment" {
-  role = aws_iam_role.weather_raw_lambda_role.name
-  policy_arn = aws-iam-policy.weather_raw_lambda_policy.arn
+resource "aws_iam_role_policy_attachment" "weather_raw_lambda_role_policy_attachment" {
+  role       = aws_iam_role.weather_raw_lambda_role.name
+  policy_arn = aws_iam_policy.weather_raw_lambda_policy.arn
 }
 
-resource "aws-lambda-function" "weather_raw_lambda" {
+data "archive_file" "weather_raw_lambda" {
+  type        = "zip"
+  source_dir  = "./weather_raw_lambda"
+  output_path = "weather_raw_lambda.zip"
+}
+
+resource "aws_lambda_function" "weather_raw_lambda" {
   function_name = "weather-raw-lambda"
-  filename = "weather_raw_lambda.zip"
-  handler = "weather_raw_lambda.lambda_handler"
-  runtime = "python3.12"
-  role = aws_iam_role.weather_raw_lambda_role.arn
-  timeout = 300
-  memory_size = 128
+  filename      = data.archive_file.weather_raw_lambda.output_path
+  handler       = "weather_raw_lambda.lambda_handler"
+  runtime       = "python3.9"
+  role          = aws_iam_role.weather_raw_lambda_role.arn
+  timeout       = 300
+  memory_size   = 128
   environment {
     variables = {
       BUCKET_NAME = var.bronze_bucket_name
     }
   }
+}
+
+resource "aws_cloudwatch_event_rule" "weather_raw_lambda_event_rule" {
+  name        = "weather-raw-lambda-event-rule"
+  description = "Event rule for weather raw lambda"
+  schedule_expression = "cron(0 12 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "weather_raw_lambda_event_target" {
+  rule      = aws_cloudwatch_event_rule.weather_raw_lambda_event_rule.name
+  arn       = aws_lambda_function.weather_raw_lambda.arn
+  target_id = "weather-raw-lambda-event-target"
+}
+
+resource "aws_lambda_permission" "weather_raw_lambda_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.weather_raw_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.weather_raw_lambda_event_rule.arn
 }
--- weather_raw_lambda/weather_raw_lambda.py
+++ weather_raw_lambda/weather_raw_lambda.py
@@ -6,6 +6,8 @@ import os
 
 BUCKET_NAME = os.environ["BUCKET_NAME"]
 
+s3 = boto3.client("s3")
+
 def lambda_handler(event, context):
     """
     Lambda function to fetch weather data from OpenWeatherMap API and save to S3
@@ -17,3 +19,19 @@ def lambda_handler(event, context):
 
     raw_data["extraction_timestamp"] = datetime.datetime.now().isoformat()
 
+    filename = f"weather_raw_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
+
+    s3.put_object(
+        Bucket=BUCKET_NAME,
+        Key=f"raw/{filename}",
+        Body=json.dumps(raw_data).encode(),
+        ContentType="application/json"
+    )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Weather data fetched and saved to S3",
+            "filename": filename
+        })
+    }
